9f70a562243ec02a5a2488feae8eccb22f25304e
HiruKub <ping.k2550@gmail.com>
2026-03-11 03:29:51 +0700

    fix

--- system_class.py
+++ system_class.py
@@ -1257,6 +1257,7 @@ class Clinic:
             # เผื่อใส่ไม่ตรง format
             room_type = room_type.lower()
             # Not have VIP or let Vip be privateroom
+            # We only have privateroom and shareroom.
             if room_type == "private":
                 room_type = "privateroom"
             elif room_type == "share":
